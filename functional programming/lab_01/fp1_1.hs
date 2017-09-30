eeb2fc08a969f1b6a294aadba3ea9c55c3b0072b
Denis Kruminsh <ipsaer@gmail.com>
2017-09-30 20:51:52 +0300

    some fix

--- functional programming/lab_01/fp1_1.hs
+++ functional programming/lab_01/fp1_1.hs
@@ -1,9 +1,11 @@
 data Operator = Plus | Minus | Mult deriving (Show, Eq)
 
+data UnaryOperator = UnMinus deriving (Show, Eq)
+
 data Term = IntConstant { intValue::Int }
           | Variable { varName::String }
           | BinaryTerm { lhv::Term, rhv::Term, op::Operator } 
-          | UnaryTerm {op::Operator, value::Term} deriving (Show, Eq)
+          | UnaryTerm {unaryOp::UnaryOperator, value::Term} deriving (Show, Eq)
 
 infixl 6 <+>
 (IntConstant a) <+> (IntConstant b) = IntConstant (a + b)
@@ -16,6 +18,10 @@ a <-> b = BinaryTerm a b Minus
 infixl 7 <*>
 (IntConstant a) <*> (IntConstant b) = IntConstant (a * b)
 a <*> b = BinaryTerm a b Mult
+
+infixl 8 <-->
+(IntConstant a) = IntConstant (-a)
+(<-->) a = UnaryTerm UnMinus a
 
 replaceVar :: Term -> String -> Term -> Term
 replaceVar (IntConstant intValue) _ _ = IntConstant intValue
